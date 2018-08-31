63f01acea72fda5696425381a5411d8929b44b1a
natan <natansidjabat@gmail.com>
2018-08-31 17:05:59 +0700

    update

--- resources/views/users/datatables_actions.blade.php
+++ resources/views/users/datatables_actions.blade.php
@@ -1,10 +1,8 @@
 {!! Form::open(['route' => ['users.destroy', $id], 'method' => 'delete']) !!}
 <div class='btn-group'>
     <a href="{{ route('users.show', $id) }}" class='btn btn-default btn-xs'>
-        <i class="glyphicon glyphicon-eye-open"></i>
-    </a>
-    <a href="{{ route('users.edit', $id) }}" class='btn btn-default btn-xs'>
-        <i class="glyphicon glyphicon-edit"></i>
+        <i class="glyphicon glyphicon-eye-open"></i> </a>
+    <a href="{{ route('users.edit', $id) }}" class='btn btn-default btn-xs'> <i class="glyphicon glyphicon-edit"></i>
     </a>
     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', [
         'type' => 'submit',
